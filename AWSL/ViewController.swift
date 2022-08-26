605a2fca0b6785eb9257a39e60ab2c66cca5b7e9
FlyKite <DogeFlyKite@gmail.com>
2022-08-26 12:35:42 +0800

    Initial commit

---
+++ AWSL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AWSL
+//
+//  Created by FlyKite on 2022/8/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
